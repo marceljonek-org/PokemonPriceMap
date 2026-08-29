d0b47a5deec9db47a192f7ef860e746f013506b9
marceljonek-org <marcel.jonek@gmail.com>
2026-08-29 18:42:17 +0200

    Update scrape.py

--- src/scrape.py
+++ src/scrape.py
@@ -35,7 +35,6 @@ HEADERS = {
     "Accept": ("text/html,application/xhtml+xml,application/xml;q=0.9,"
                "image/avif,image/webp,*/*;q=0.8"),
     "Accept-Language": "sk-SK,sk;q=0.9,cs;q=0.8,en-US;q=0.7,en;q=0.6",
-    "Accept-Encoding": "gzip, deflate, br",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
